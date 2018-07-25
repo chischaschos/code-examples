780dcbffd9a9a61ac1a86a705712881d6e23f05e
emmanuel delgado <larin.s931@gmail.com>
2018-07-25 15:14:36 -0700

    Add some unusual strings creation examples

---
+++ ruby-examples/strings.rb
@@ -0,0 +1,39 @@
+a = "a"
+puts a
+
+a = "b" "b"
+puts a
+
+a = "c""c"
+puts a
+
+a = """"
+puts a
+
+a = """d"
+puts a
+
+a = "
+e
+"
+puts a
+
+a = "f""
+f
+"
+puts a
+
+a = """
+g
+"
+puts a
+
+a = "h""
+h
+""h"
+puts a
+
+a = """
+i
+"""
+puts a
