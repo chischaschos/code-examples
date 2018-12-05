9b3608f37ac64dfc78ac635b37f96f19202b6ce1
emmanuel delgado <emmanuel.delgado@juvo.com>
2018-12-05 15:20:04 -0800

    Add an example of ruby inheritance

--- ruby-examples/inheritance.rb
+++ ruby-examples/inheritance.rb
@@ -9,3 +9,37 @@ end
 
 
 puts B.new.bye
+
+module Houses
+  class Tell
+    def do
+      "saying"
+    end
+  end
+
+  class Red
+    def tell
+      tell_class.new.do
+    end
+
+    def tell_class
+      Tell
+    end
+  end
+end
+
+module Cars
+  class Tell
+    def do
+      "saying cars"
+    end
+  end
+
+  class Red < Houses::Red
+    def tell_class
+      Tell
+    end
+  end
+end
+
+puts Cars::Red.new.tell
