30696741174bc631795e2485a08e0bd7186768f6
chischaschos <larin.s931@gmail.com>
2019-10-09 09:50:31 -0700

    Forgot to commit this completed piece

--- ruby-examples/adventofcode.com-2018/day3.rb
+++ ruby-examples/adventofcode.com-2018/day3.rb
@@ -1,5 +1,6 @@
 claims = ARGF.read.split("\n")
 sqs = Hash.new { |h, k| h[k] = [] }
+ids = {}
 
 claims.each do |c|
   id, _, edges, area = c.split
@@ -13,10 +14,19 @@ claims.each do |c|
   (x...x + w).each do |xc|
     (y...y + h).each do |xy|
       sqs["#{xc},#{xy}"] << id
+      ids[id] = true
     end
   end
 end
 
-matches = sqs.select { |_, v| v.length > 1 }
+t_sqs = 0
 
-puts matches.size
+sqs.each do |_, ids_per_pos|
+  if ids_per_pos.length > 1
+    t_sqs += 1
+    ids_per_pos.each { |id| ids[id] = false }
+  end
+end
+
+puts t_sqs
+puts ids.find { |_, v| v }.first
